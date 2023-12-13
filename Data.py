56d175f778c42c9b8e23aeee1e554689dcaa07a6
mansouan <antonin.mansour@edu.esiee.fr>
2023-12-13 14:09:28 +0100

    add comments

--- Data.py
+++ Data.py
@@ -2,15 +2,23 @@ import pandas as pd
 from io import StringIO
 import requests
 
-def getDataFrame():
-    # fetching csv
-    response = requests.get("https://www.data.gouv.fr/fr/datasets/r/bc9d5d13-07cc-4d38-8254-88db065bd42b").text
+def getDataFrame(url):
+    """Récupération des données CSV et création du dataFrame
 
-    # create dataframe
+    Args:
+        url (str): URL pour récupérer les données en CSV.
+
+    Returns:
+        DataFrame: dataFrame des prix moyen du loyer par département
+    """
+    # Récupération du csv
+    response = requests.get(url).text
+
+    # création du DataFrame
     csv_buffer = StringIO(response)
     dataFrame = pd.read_csv(csv_buffer, sep=";", encoding="latin-1")
 
-    # Convertir la colonne 'loypredm2' en float
+    # Convertir la colonne 'loypredm2' (prix moyen du loyer) en float
     dataFrame["loypredm2"] = dataFrame["loypredm2"].str.replace(",", ".").astype(float)
 
     return dataFrame
